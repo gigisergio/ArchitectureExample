7fe15cf689ca9116d97ca19f546c3c43aa7b914c
Eduardo González <egonzalez@iphonedroid.com>
2018-04-14 09:32:29 +0200

    Initial Commit

---
+++ ArchitectureExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchitectureExample
+//
+//  Created by Eduardo González on 14/4/18.
+//  Copyright © 2018 Eduardo González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
